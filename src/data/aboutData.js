5e7f2d476be76a88c402350a9d61bc722e121ae3
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-13 23:15:50 +0700

    Revise service items and contact details in aboutData.js
    Updated service items and contact information to reflect new educational services and details in Indonesian.

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -11,48 +11,48 @@ export const serviceItems = [
   {
     id: "1",
     icon: "/images/icons/icon-1.svg",
-    title: "Ui/Ux Design",
-    des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
+    title: "Pembelajaran Interaktif dan Efektif",
+    des: "Menyediakan layanan pengajaran IPA dan Fisika dengan pendekatan yang mudah dipahami, interaktif, dan berorientasi pada kebutuhan peserta didik.",
     color: "#D566FF",
     bg: "#FCF4FF"
   },
   {
     id: "2",
     icon: "/images/icons/icon-2.svg",
-    title: "App Development",
-    des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
+    title: "Website Informasi Pendidikan",
+    des: "Membantu sekolah, madrasah, maupun yayasan dalam merancang, mengelola, dan memperbarui website sebagai media informasi.",
     color: "#DDA10C",
     bg: "#FEFAF0"
   },
   {
     id: "3",
     icon: "/images/icons/icon-3.svg",
-    title: "Photography",
-    des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
+    title: "Desain Materi dan Konten Edukasi",
+    des: "Mengembangkan media pembelajaran berbasis digital seperti presentasi interaktif, infografis, video edukasi, dan simulasi pembelajaran.",
     color: "#8774FF",
     bg: "#FCF4FF"
   },
   {
     id: "4",
     icon: "/images/icons/icon-4.svg",
-    title: "Photography",
-    des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
+    title: "Pengelolaan Data Akademik",
+    des: "Berpengalaman dalam pengelolaan sistem ujian berbasis komputer, penginputan nilai akademik, sinkronisasi data pendidikan.",
     color: "#FF6080",
     bg: "#FFF4F4"
   },
   {
     id: "5",
     icon: "/images/icons/icon-5.svg",
-    title: "Managment",
-    des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
+    title: "Pelatihan Teknologi Pendidikan",
+    des: "Memberikan pendampingan dan pelatihan penggunaan teknologi pendidikan seperti Google Classroom, Zoom Meeting, Canva, Microsoft Office, dan berbagai platform pembelajaran digital.",
     color: "#FF75D8",
     bg: "#FFF0F8"
   },
   {
     id: "6",
     icon: "/images/icons/icon-6.svg",
-    title: "Web Development",
-    des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
+    title: "Memberikan Motivasi Kepada Anak",
+    des: "Menyiapkan anak - anak pembekalan mental yang sehat, agar mereka tertarik dan tidak mendapatkan beban terutama saat menghadapi pelajaran eksak.",
     color: "#269FFF",
     bg: "#F3FAFF"
   }
@@ -63,15 +63,15 @@ export const personalInfoData = [
     id: 1,
     iconType: "phone",
     iconColor: "text-oriange",
-    name: "Phone",
+    name: "Telepon",
     meta: <a className="hover:text-[#FA5252] duration-300 transition" href="tel:+62887433130861">+62 8874 3313 0861</a>
   },
   {
     id: 2,
     iconType: "map",
     iconColor: "text-oriange-lite",
-    name: "Location",
-    meta: "Jungkel Rajeg"
+    name: "Alamat",
+    meta: "Jungkel, Rajeg"
   },
   {
     id: 3,
@@ -84,7 +84,7 @@ export const personalInfoData = [
     id: 4,
     iconType: "calendar",
     iconColor: "text-color-50",
-    name: "Birthday",
-    meta: "May 27, 1990"
+    name: "Tanggal Lahir",
+    meta: "01 Januari 1990"
   }
 ]
